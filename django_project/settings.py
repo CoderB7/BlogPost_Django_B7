b2201d554abec1abb4ccf7c9402c45c76aa5ad83
CoderB7 <iutstudent2022@gmail.com>
2024-01-10 12:15:45 +0500

    Set env variables

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,19 +12,20 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s83k5$stt)h_rrr(3$5eq6%@zlk$)kk9mu2%777qz&&(v)_i3g'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (bool(int(os.environ.get('DEBUG', 1))))
 
 ALLOWED_HOSTS = []
 
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogpostdb',
+        'USER': 'userb',
+        'PASSWORD': 'cyber_killer',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
